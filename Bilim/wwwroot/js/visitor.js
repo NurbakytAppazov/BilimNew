1c5e56e251ba68bc63b8d9a834ad310ff13b286d
NurbakytAppazov <nurbakyt0970@gmail.com>
2020-08-22 23:40:30 +0600

    22.08.2020 – songy

--- Bilim/wwwroot/js/visitor.js
+++ Bilim/wwwroot/js/visitor.js
@@ -1,44 +1,15 @@
 $(document).ready(function () {
-
-
-
-	$(window).on('scroll', function() {
-		if($(window).scrollTop() > 90){
-        $('.header').addClass('fix-header');
-		} else{
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 90) {
+			$('.header').addClass('fix-header');
+		} else {
 			$('.header').removeClass('fix-header');
 		}
 	});
 
 
 
-    $(window).on('scroll', function() {
-		if($(window).scrollTop() >= $('#main').offset().top){
-			$('a.fix-active').removeClass('fix-active');
-			$('.main-nav').addClass('fix-active');
-		}
-		if ($(window).scrollTop() >= $('#teacher').offset().top) {
-			$('a.fix-active').removeClass('fix-active');
-			$('.teacher-nav').addClass('fix-active');
-		}
-		if($(window).scrollTop() >= $('#about').offset().top){
-			$('a.fix-active').removeClass('fix-active');
-			$('.about-nav').addClass('fix-active');
-		}
-		if($(window).scrollTop() >= $('#courses').offset().top){
-			$('a.fix-active').removeClass('fix-active');
-			$('.course-nav').addClass('fix-active');
-		}
-		if ($(window).scrollTop() >= $('.packages').offset().top - 100) {
-			$('a.fix-active').removeClass('fix-active');
-			$('.packages-nav').addClass('fix-active');
-		}
-	});
-
-
-
-
-	$('.main-nav').click(function() {
+	$('.main-nav').click(function () {
 		var offset = 0;
 		$('a.fix-active').removeClass('fix-active');
 		$('.main-nav').addClass('fix-active');
@@ -47,7 +18,7 @@ $(document).ready(function () {
 		}, 1000);
 		return false;
 	});
-	$('.about-nav').click(function() {
+	$('.about-nav').click(function () {
 		var offset = 0;
 		$('a.fix-active').removeClass('fix-active');
 		$('.about-nav').addClass('fix-active');
@@ -56,7 +27,7 @@ $(document).ready(function () {
 		}, 1000);
 		return false;
 	});
-	$('.course-nav, .select-btn').click(function() {
+	$('.course-nav, .select-btn').click(function () {
 		var offset = 0;
 		$('a.fix-active').removeClass('fix-active');
 		$('.course-nav').addClass('fix-active');
@@ -65,24 +36,4 @@ $(document).ready(function () {
 		}, 1000);
 		return false;
 	});
-	$('.teacher-nav').click(function () {
-		var offset = 0;
-		$('a.fix-active').removeClass('fix-active');
-		$('.teacher-nav').addClass('fix-active');
-		$('html, body').animate({
-			scrollTop: $('#teacher').offset().top
-		}, 1000);
-		return false;
-	});
-	$('.packages-nav').click(function () {
-		var offset = 0;
-		$('a.fix-active').removeClass('fix-active');
-		$('.packages-nav').addClass('fix-active');
-		$('html, body').animate({
-			scrollTop: $('.packages').offset().top - 100
-		}, 1000);
-		return false;
-	});
-         
-         
 });
